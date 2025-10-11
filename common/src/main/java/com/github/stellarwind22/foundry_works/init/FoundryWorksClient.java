d3f2fee4c5bde01e35834e32bb0d43de25936924
StellarWind22 <gannonburks45@gmail.com>
2025-10-10 19:37:09 -0500

    added .mcmeta files for lanterns.

--- common/src/main/java/com/github/stellarwind22/foundry_works/init/FoundryWorksClient.java
+++ common/src/main/java/com/github/stellarwind22/foundry_works/init/FoundryWorksClient.java
@@ -8,6 +8,10 @@ public class FoundryWorksClient {
 
     public static void init() {
         RenderTypeRegistry.register(ChunkSectionLayer.CUTOUT, FoundryWorksBlocks.GOLD_CHAIN.get());
+        RenderTypeRegistry.register(ChunkSectionLayer.CUTOUT, FoundryWorksBlocks.GOLD_BARS.get());
+        RenderTypeRegistry.register(ChunkSectionLayer.CUTOUT, FoundryWorksBlocks.GOLD_LANTERN.get());
         RenderTypeRegistry.register(ChunkSectionLayer.CUTOUT, FoundryWorksBlocks.NETHERITE_CHAIN.get());
+        RenderTypeRegistry.register(ChunkSectionLayer.CUTOUT, FoundryWorksBlocks.NETHERITE_BARS.get());
+        RenderTypeRegistry.register(ChunkSectionLayer.CUTOUT, FoundryWorksBlocks.NETHERITE_LANTERN.get());
     }
 }
