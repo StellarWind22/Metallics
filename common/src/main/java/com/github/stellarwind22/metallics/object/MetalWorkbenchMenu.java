6a9611e927041a96616a91d0c5bc96668f78b8af
StellarWind22 <gannonburks45@gmail.com>
2025-12-04 14:26:42 -0600

    messed around with workbench more

--- common/src/main/java/com/github/stellarwind22/metallics/object/MetalWorkbenchMenu.java
+++ common/src/main/java/com/github/stellarwind22/metallics/object/MetalWorkbenchMenu.java
@@ -4,21 +4,23 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class MetalWorkbenchMenu extends AbstractContainerMenu {
 
     public static final int INP_SLOT_1 = 0;
     public static final int INP_SLOT_2 = 1;
-    public static final int FUEL_SLOT = 3;
-    public static final int OUT_SLOT = 4;
+    public static final int INP_SLOT_3 = 2;
+    public static final int FUEL_SLOT = 4;
+    public static final int OUT_SLOT = 5;
 
     protected MetalWorkbenchMenu(@Nullable MenuType<?> menuType, int i) {
         super(menuType, i);
     }
 
     @Override
-    public ItemStack quickMoveStack(Player player, int i) {
+    public @NotNull ItemStack quickMoveStack(Player player, int i) {
         return null;
     }
 
--- common/src/main/java/com/github/stellarwind22/metallics/object/MetalWorkbenchScreen.java
+++ common/src/main/java/com/github/stellarwind22/metallics/object/MetalWorkbenchScreen.java
@@ -5,9 +5,9 @@ import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 
-public class MetalWorkbenchBlockEntity extends AbstractContainerScreen<MetalWorkbenchMenu> {
+public class MetalWorkbenchScreen extends AbstractContainerScreen<MetalWorkbenchMenu> {
 
-    public MetalWorkbenchBlockEntity(MetalWorkbenchMenu metalWorkbenchMenu, Inventory inventory, Component component) {
+    public MetalWorkbenchScreen(MetalWorkbenchMenu metalWorkbenchMenu, Inventory inventory, Component component) {
         super(metalWorkbenchMenu, inventory, component);
     }
 
